4c513c244e19533d120af943a0214972941066d9
rnshalinda <nsrhewa@gmail.com>
2025-09-12 21:00:25 +0530

    deleted old error files

--- ThogaKade/src/main/java/edu/icet/cims/Main.java
+++
@@ -1,7 +0,0 @@
-package edu.icet.cims;
-
-public class Main {
-    public static void main(String[] args) {
-        Starter.main(args);
-    }
-}
--- ThogaKade/src/main/java/edu/icet/cims/service/ServiceCredential.java
+++
@@ -1,9 +0,0 @@
-package edu.icet.cims.service;
-
-import edu.icet.cims.model.dto.UserCredentialsDto;
-import edu.icet.cims.service.exception.UserCredentialsException;
-
-public interface ServiceCredential {
-
-    void credentialsValidate(UserCredentialsDto credDTO) throws UserCredentialsException;
-}
